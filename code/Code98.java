8071315c524668cf728b847106c3872ffc85bdcf
YoungHnhb <hnhbliyueyang@gmail.com>
2020-04-15 13:14:31 +0800

    add code 98 file

--- code/Code98.java
+++ code/Code98.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /*
 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
 
@@ -8,4 +10,53 @@
 所有左子树和右子树自身必须也是二叉搜索树。
  */
 public class Code98 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        double inorder = - Double.MAX_VALUE;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.val <= inorder) {
+                return false;
+            }
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+
+    /**
+     * 第二种解法
+     */
+    private long lastVal = Long.MIN_VALUE;
+    public boolean isValidBST_New(TreeNode root) {
+        return dfs(root);
+    }
+
+    private boolean dfs(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!dfs(root.left)) {
+            return false;
+        }
+        if (root.val <= lastVal) {
+            return false;
+        }
+        lastVal = (long) root.val;
+        if (!dfs(root.right)) {
+            return false;
+        }
+        return true;
+    }
+
 }
